70258583b20d121fc53907963ac34dff3529c7d0
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-25 12:29:10 +1000

    Create project

---
+++ src/Level1.java
@@ -0,0 +1,11 @@
+public class Level1 {
+    public static void MatrixTurn(String[] Matrix, int m, int n, int t) {
+
+    }
+
+    public static void main(String[] args) {
+        MatrixTurn(new String[] {"123456", "234567", "345678", "456789"}, 4, 6, 3);
+    }
+}
+
+
