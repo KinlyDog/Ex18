56699247280792d1e76427fb74b5643e1c78b5ba
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-26 21:25:00 +1000

    Update project

--- src/Level1.java
+++ src/Level1.java
@@ -1,10 +1,7 @@
 public class Level1 {
-    public static void MatrixTurn(String[] matrix, int x, int y, int r) {
+    public static void MatrixTurn(String[] matrix, int x, int y, int t) {
         char[][] work = new char[x][y];
         char[][] temp = new char[x][y];
-
-        int xLen = x - 1;
-        int yLen = y - 1;
 
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
@@ -13,13 +10,17 @@ public class Level1 {
             }
         }
 
-        for (int round = 0; round < r; round++) {
-            int i = 0;
+        int xLen = x - 1;
+        int yLen = y - 1;
+
+        for (int step = 0; step < t; step++) {
+            int i;
             int j;
 
             for (int count = 0; count < x / 2; count++) {
+
                 for (j = count; j < yLen - count; j++) {
-                    work[i][j + 1] = temp[i][j];
+                    work[count][j + 1] = temp[count][j];
                 }
 
                 for (i = count; i < xLen - count; i++) {
@@ -33,8 +34,6 @@ public class Level1 {
                 for (i = xLen - count; i > count; i--) {
                     work[i - 1][j] = temp[i][j];
                 }
-
-                i++;
             }
 
             for (i = 0; i < x; i++) {
